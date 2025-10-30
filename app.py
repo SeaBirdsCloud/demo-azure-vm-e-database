36c4cfa4600447669c9342602a85dc6d7a04bf1b
Perdix <perolive.192@gmail.com>
2025-10-30 16:10:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -226,4 +226,5 @@ def query(db):
     return render_template('query.html', db=db, message=message)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
+
